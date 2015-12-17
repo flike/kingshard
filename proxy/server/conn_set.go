2fce66275cdfd737c2f8b3f004c48098091ca8eb
flike <flikecn@126.com>
2015-12-17 09:22:51 +0800

    bugfix[set autocommit]

--- proxy/server/conn_set.go
+++ proxy/server/conn_set.go
@@ -57,17 +57,16 @@ func (c *ClientConn) handleSetAutoCommit(val sqlparser.ValExpr) error {
 		c.status |= mysql.SERVER_STATUS_AUTOCOMMIT
 		if c.status&mysql.SERVER_STATUS_IN_TRANS > 0 {
 			c.status &= ^mysql.SERVER_STATUS_IN_TRANS
-
-			for _, co := range c.txConns {
-				if e := co.SetAutoCommit(1); e != nil {
-					co.Close()
-					c.txConns = make(map[*backend.Node]*backend.BackendConn)
-					return fmt.Errorf("set autocommit error, %v", e)
-				}
-				co.Close()
-			}
-			c.txConns = make(map[*backend.Node]*backend.BackendConn)
 		}
+		for _, co := range c.txConns {
+			if e := co.SetAutoCommit(1); e != nil {
+				co.Close()
+				c.txConns = make(map[*backend.Node]*backend.BackendConn)
+				return fmt.Errorf("set autocommit error, %v", e)
+			}
+			co.Close()
+		}
+		c.txConns = make(map[*backend.Node]*backend.BackendConn)
 	case '0':
 		c.status &= ^mysql.SERVER_STATUS_AUTOCOMMIT
 	default:
