74f14b279d46aeac697a56697c07d1b6d79484ef
flike <flikecn@126.com>
2016-01-26 18:54:34 +0800

    support charset

--- proxy/server/conn.go
+++ proxy/server/conn.go
@@ -157,7 +157,8 @@ func (c *ClientConn) writeInitialHandshake() error {
 	data = append(data, byte(DEFAULT_CAPABILITY), byte(DEFAULT_CAPABILITY>>8))
 
 	//charset, utf-8 default
-	data = append(data, uint8(mysql.DEFAULT_COLLATION_ID))
+	//data = append(data, uint8(mysql.DEFAULT_COLLATION_ID))
+	data = append(data, uint8(mysql.CharsetIds[c.charset]))
 
 	//status
 	data = append(data, byte(c.status), byte(c.status>>8))
