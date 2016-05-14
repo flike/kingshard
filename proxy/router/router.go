bfc53302e3cbd23a9ccd8b21d75495c0eb7bf92d
flike <flikecn@126.com>
2016-05-14 09:30:53 +0800

    bugfix #165

--- proxy/router/router.go
+++ proxy/router/router.go
@@ -488,10 +488,12 @@ func (r *Router) rewriteSelectSql(plan *Plan, node *sqlparser.Select, tableIndex
 	)
 
 	var prefix string
-	//for shardTable.*,need replace table into shardTable_xxxx.
+	//rewrite select expr
 	for _, expr := range node.SelectExprs {
-		if starExpr, ok := expr.(*sqlparser.StarExpr); ok {
-			if string(starExpr.TableName) == plan.Rule.Table {
+		switch v := expr.(type) {
+		case *sqlparser.StarExpr:
+			//for shardTable.*,need replace table into shardTable_xxxx.
+			if string(v.TableName) == plan.Rule.Table {
 				fmt.Fprintf(buf, "%s%s_%04d.*",
 					prefix,
 					plan.Rule.Table,
@@ -500,7 +502,28 @@ func (r *Router) rewriteSelectSql(plan *Plan, node *sqlparser.Select, tableIndex
 			} else {
 				buf.Fprintf("%s%v", prefix, expr)
 			}
-		} else {
+		case *sqlparser.NonStarExpr:
+			//rewrite shardTable.column as a
+			//into shardTable_xxxx.column as a
+			if colName, ok := v.Expr.(*sqlparser.ColName); ok {
+				if string(colName.Qualifier) == plan.Rule.Table {
+					fmt.Fprintf(buf, "%s%s_%04d.%s",
+						prefix,
+						plan.Rule.Table,
+						tableIndex,
+						string(colName.Name),
+					)
+				} else {
+					buf.Fprintf("%s%v", prefix, colName)
+				}
+				//if expr has as
+				if v.As != nil {
+					buf.Fprintf(" as %s", v.As)
+				}
+			} else {
+				buf.Fprintf("%s%v", prefix, expr)
+			}
+		default:
 			buf.Fprintf("%s%v", prefix, expr)
 		}
 		prefix = ", "
