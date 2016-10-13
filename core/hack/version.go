a26e5ec1d75f0d23e392409b9699f2f5688d6554
zhangliang3 [张良] <zhangliang3@xiaomi.com>
2016-10-13 17:01:30 +0800

    SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci
        https://github.com/flike/kingshard/issues/257

--- core/hack/version.go
+++ core/hack/version.go
@@ -1,6 +1,6 @@
 package hack
 
 const (
-	Version = "2016-09-23 15:44:15 +0800 @a15fd20"
-	Compile = "2016-09-24 20:32:20 +0800 by go version go1.7.1 darwin/amd64"
+	Version = "2016-09-25 08:29:17 +0800 @3725dd4"
+	Compile = "2016-10-13 16:53:26 +0800 by go version go1.7.1 linux/amd64"
 )
