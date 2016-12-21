0868fceff59ea8fc286c42329fd0515a66ddad93
flike <flikecn@126.com>
2016-12-21 20:47:13 +0800

    bugfix #300

--- core/hack/version.go
+++ core/hack/version.go
@@ -1,6 +1,6 @@
 package hack
 
 const (
-	Version = "2016-12-11 09:36:57 +0800 @da96aa1"
-	Compile = "2016-12-14 20:00:54 +0800 by go version go1.7.3 darwin/amd64"
+	Version = "2016-12-21 14:18:21 +0800 @25b54bc"
+	Compile = "2016-12-21 20:41:54 +0800 by go version go1.7.3 darwin/amd64"
 )
