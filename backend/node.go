e3faa9292be3d34484bc7cff9c29fd259211731e
flike <flikecn@126.com>
2017-10-21 16:18:59 +0800

    bugfix #387

--- backend/node.go
+++ backend/node.go
@@ -103,7 +103,11 @@ func (n *Node) checkMaster() {
 	} else {
 		if atomic.LoadInt32(&(db.state)) == Down {
 			golog.Info("Node", "checkMaster", "Master up", 0, "db.Addr", db.Addr())
-			n.UpMaster(db.addr)
+			err := n.UpMaster(db.addr)
+			if err != nil {
+				golog.Error("Node", "checkMaster", "UpMaster", 0, "db.Addr", db.Addr(), "error", err.Error())
+				return
+			}
 		}
 		db.SetLastPing()
 		if atomic.LoadInt32(&(db.state)) != ManualDown {
@@ -252,6 +256,7 @@ func (n *Node) UpMaster(addr string) error {
 	db, err := n.UpDB(addr)
 	if err != nil {
 		golog.Error("Node", "UpMaster", err.Error(), 0)
+		return err
 	}
 	n.Master = db
 	return err
