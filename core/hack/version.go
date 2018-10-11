6f43a65e5ef48a7f08dd4993628ffd791d27208a
zhangsong <zhangsong@taqu.cn>
2018-10-11 20:01:18 +0800

    修复tryReuse时可能出现的无法连接问题

--- core/hack/version.go
+++ core/hack/version.go
@@ -1,6 +1,6 @@
 package hack
 
 const (
-	Version = "2018-09-07 09:17:26 +0800 @50b5411"
-	Compile = "2018-09-08 11:30:50 +0800 by go version go1.10.2 darwin/amd64"
+	Version = "2018-09-08 11:38:27 +0800 @90db8ab"
+	Compile = "2018-10-11 19:57:54 +0800 by go version go1.9 darwin/amd64"
 )
